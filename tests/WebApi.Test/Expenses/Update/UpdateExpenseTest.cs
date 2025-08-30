c3e891551fd8ea02dde285daba9a3ff0cac77c5f
Ueverton95 <uevertonmtmdev@outlook.com>
2025-08-29 22:31:27 -0300

    remover test

--- tests/WebApi.Test/Expenses/Update/UpdateExpenseTest.cs
+++ tests/WebApi.Test/Expenses/Update/UpdateExpenseTest.cs
@@ -34,35 +34,35 @@ public class UpdateExpenseTest : CashFlowClassFixture
         
         result.StatusCode.Should().Be(HttpStatusCode.NoContent);
     }
-    [Fact]
-    public async Task Error_Title_Empty()
-    {
-        var request = RequestExpenseJsonBuilder.Build();
-        request.Title = string.Empty;
+    //[Fact]
+    //public async Task Error_Title_Empty()
+    //{
+    //    var request = RequestExpenseJsonBuilder.Build();
+    //    request.Title = string.Empty;
 
-        var result = await DoPut(requestUri: $"{METHOD}/{_expenseId}", request: request, token: _token);
-        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    //    var result = await DoPut(requestUri: $"{METHOD}/{_expenseId}", request: request, token: _token);
+    //    result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
 
-        var body = await result.Content.ReadAsStreamAsync();
-        var response = await JsonDocument.ParseAsync(body);
-        var errors = response.RootElement.GetProperty("errorMessage").EnumerateArray();
-        var expectedMessage = ResourceErrorMessages.TITLE_REQUIRED;
+    //    var body = await result.Content.ReadAsStreamAsync();
+    //    var response = await JsonDocument.ParseAsync(body);
+    //    var errors = response.RootElement.GetProperty("errorMessage").EnumerateArray();
+    //    var expectedMessage = ResourceErrorMessages.TITLE_REQUIRED;
 
-        errors.Should().HaveCount(1).And.Contain(error => error.GetString()!.Equals(expectedMessage));
-    }
-    [Fact]
-    public async Task Error_Expense_Not_Found()
-    {
-        var request = RequestExpenseJsonBuilder.Build();
+    //    errors.Should().HaveCount(1).And.Contain(error => error.GetString()!.Equals(expectedMessage));
+    //}
+    //[Fact]
+    //public async Task Error_Expense_Not_Found()
+    //{
+    //    var request = RequestExpenseJsonBuilder.Build();
 
-        var result = await DoPut(requestUri: $"{METHOD}/1000", request: request, token: _token);
-        result.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    //    var result = await DoPut(requestUri: $"{METHOD}/1000", request: request, token: _token);
+    //    result.StatusCode.Should().Be(HttpStatusCode.NotFound);
 
-        var body = await result.Content.ReadAsStreamAsync();
-        var response = await JsonDocument.ParseAsync(body);
-        var errors = response.RootElement.GetProperty("errorMessage").EnumerateArray();
-        var expectedMessage = ResourceErrorMessages.EXPENSE_NOT_FOUND;
+    //    var body = await result.Content.ReadAsStreamAsync();
+    //    var response = await JsonDocument.ParseAsync(body);
+    //    var errors = response.RootElement.GetProperty("errorMessage").EnumerateArray();
+    //    var expectedMessage = ResourceErrorMessages.EXPENSE_NOT_FOUND;
 
-        errors.Should().HaveCount(1).And.Contain(error => error.GetString()!.Equals(expectedMessage));
-    }
+    //    errors.Should().HaveCount(1).And.Contain(error => error.GetString()!.Equals(expectedMessage));
+    //}
 }
